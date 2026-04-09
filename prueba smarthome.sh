6492f42f1c14f51581a2dfab4f9735d9de478af6
Nicolas Luque <nicolasluque0013@gmail.com>
2026-04-08 23:40:42 -0300

    se agrega codigo para futuras pruebas

--- prueba smarthome.sh
+++ prueba smarthome.sh
@@ -1,15 +1,33 @@
-//Detección de temperatura alta y encender el aire en 16
+## Rutina del gil laburante: 
+## abrir la puerta 
+## si la alarma está encendida, que la apague.
+## el sensor de movimiento detecta movimiento en la entrada y prende el foco del living y la cocina en blanco al 100%. 
+## prende el aire de la pieza en 16. Si la persiana de la pieza está al 100%, la baja por completo al 0%.
+ 
+BEGIN "Gil laburante"
+    WHEN cerradura_porton.estado == ON DO
+        IF alarma_entrada.estado == ON THEN
+            alarma_entrada.activada = OFF
+        END
+        IF sensor_movimiento_entrada == TRUE THEN
+            foco_living.estado = ON
+            foco_living.brillo = 100%
+            foco_living.color = blanco
+            foco_cocina.estado = ON
+            foco_cocina.brillo = 100%
+            foco_cocina.color = blanco
+            aire_pieza.estado = ON
+            aire_pieza.modo = FRIO
+            aire_pieza.temp_act = sensor_temp_pieza
+            IF aire_pieza.temp_act > 30°C THEN
+                aire_pieza.temp_obj = 20°C
+                ELSE
+                aire_pieza.temp_obj = 24°C
+            END
 
-BEGIN "Deteccion"
-
-    WHEN sensor_temp > 32°C DO
-        aire_pieza.estado = ON
-        aire_pieza.modo = FRIO
-        aire_pieza.temp = 16°C
-        IF reloj_pieza.hora >= 12:00 THEN
-            persiana_pieza.posicióon = 50%
-            ELSE
-            persiana_pieza.posicióon = 100%
+            IF persiana_pieza.posicion == 100% THEN
+                persiana_pieza.posicion = 0%
+            END
         END
     END
 END
